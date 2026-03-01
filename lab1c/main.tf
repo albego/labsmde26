d2b5dd38be0156e755e265fd07584015329ae959
Alberto <156366418+albego38@users.noreply.github.com>
2026-03-01 22:34:06 +0100

    Create main.tf
    Fichero main de configuración para desplegar dos grupos de recursos.

---
+++ lab1c/main.tf
@@ -0,0 +1,35 @@
+# 1. Configuracion del Proveedor
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+# 2. Grupo de Recursos para DESARROLLO
+resource "azurerm_resource_group" "dev" {
+  name     = "rg-lab1btf-dev"
+  location = "West Europe"
+
+  tags = {
+    Workload = "lab1b"
+    Owner    = "tu-email@institucion.com"
+  }
+}
+
+# 3. Grupo de Recursos para PRODUCCION
+resource "azurerm_resource_group" "prod" {
+  name     = "rg-lab1btf-prod"
+  location = "West Europe"
+
+  tags = {
+    Workload = "lab1b"
+    Owner    = "tu-email@institucion.com"
+  }
+}
